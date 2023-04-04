68dd6cc2af5027d0bc04e8b5fd1209adce7d33d4
ggalushko <titanmelchior@gmail.com>
2023-04-04 13:31:27 +0300

    change focusImage function

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,15 +10,14 @@ function Hero() {
     "w-48 h-48 transition-transform duration-500 rounded-4xl bg-white";
 
   return (
-    <div className=" mt-24">
+    <div className="mt-24">
       <h1 className=" font-bold text-7xl text-left">
         Some text blabla. <br /> Yes, it is indeed a text, but longer.
       </h1>
       <div>
         <p className=" text-secondary text-left max-w-[540px]  text-xl my-10">
-          Что-то взойдет наверняка, вот тебе, друг, моя рука, солнце взойдет –
-          мне рассказал старый шаман у древних скал». Весна двоих влюбленных
-          птиц внутри шагов тепло ресниц.
+        Yes, it is indeed a text, Yes, it is indeed a text,
+        Yes, it is indeed a text,Yes, it is indeed a text, Yes, it is indeed a text,
         </p>
         <div className="ml-0 mr-auto w-fit flex gap-3">
           <button className="w-60 bg-button text-link rounded-md py-1">
@@ -29,44 +28,45 @@ function Hero() {
             TWO
           </button>
         </div>
-        <div className="flex flex-wrap justify-between h-96 just items-end px-4">
+        <div className="flex flex-wrap justify-center lg:justify-between items-end px-4 gap-14 mt-32">
           <img
             ref={img1ref}
             className={imageClass}
             onMouseEnter={() => {
-              setFocusedImage(img1ref);
+              focusImage(img1ref);
             }}
           ></img>
           <img
             ref={img2ref}
             className={imageClass}
             onMouseEnter={() => {
-              setFocusedImage(img2ref);
+              focusImage(img2ref);
             }}
           ></img>
           <img
             ref={img3ref}
             className={imageClass}
             onMouseEnter={() => {
-              setFocusedImage(img3ref);
+              focusImage(img3ref);
             }}
           ></img>
           <img
             ref={img4ref}
             className={imageClass}
             onMouseEnter={() => {
-              setFocusedImage(img4ref);
+              focusImage(img4ref);
             }}
           ></img>
         </div>
       </div>
     </div>
   );
-  function setFocusedImage(imgRef: React.RefObject<HTMLImageElement>) {
+  function focusImage(imgRef: React.RefObject<HTMLImageElement>) {
     imgRef.current!.classList.add("scale-150");
-    imgs
-      .filter((it) => it.current !== imgRef.current)
-      .map((it) => it!.current!.classList.remove("scale-150"));
+    imgs.forEach((img) => {
+      if (img.current !== imgRef.current)
+        img!.current!.classList.remove("scale-150");
+    });
   }
 }
 
