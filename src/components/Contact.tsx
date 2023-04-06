5091f482213cda19960ec547f6f1a6b575ccb47d
ggalushko <titanmelchior@gmail.com>
2023-04-06 10:18:04 +0300

    move social icons to component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,3 +1,5 @@
+import SocialIcons from "./SocialIcons";
+
 function Contact() {
   return (
     <div className="mt-40 flex flex-col md:flex-row justify-between">
@@ -7,22 +9,9 @@ function Contact() {
           Feel free to contacts us if you want more information. We would be
           glad to help you learn more about our products
         </p>
-        <div className="flex gap-5">
-          <a href="https://www.facebook.com" target="_blank" rel="noopener">
-            <img src="../src/assets/images/facebook.png" alt="facebook logo" className="w-10 h-10 bg-center bg-contain 
-            grayscale hover:grayscale-0 transition-[filter] duration-300"/>
-          </a>
-          <a href="https://www.telegram.org" target="_blank" rel="noopener">
-            <img src="../../src/assets/images/telegram.png" alt="telegram logo" className="w-10 h-10 bg-center bg-contain
-            grayscale hover:grayscale-0 transition-[filter] duration-300"/>
-          </a>
-          <a href="https://www.twitter.com" target="_blank" rel="noopener">
-            <img src="../../src/assets/images/twitter.png" alt="twitter logo" className="w-10 h-10 bg-center bg-contain
-            grayscale hover:grayscale-0 transition-[filter] duration-300"/>
-          </a>
-        </div>
+        <SocialIcons/>
       </div>
-      <form className=" bg-dark p-8 rounded-xl flex flex-col md:w-[48%] text-xl gap-4 mt-10 md:mt-0">
+      <form className=" bg-dark p-4 lg:p-8 rounded-xl flex flex-col md:w-[48%] text-xl gap-4 mt-10 md:mt-0">
         <label className="flex flex-col gap-1"> Name
             <input type="text"  placeholder="Enter name" 
             className="px-3 py-2 rounded-lg bg-transparent border border-secondary border-opacity-60 mt-2"></input>
---
+++ src/components/SocialIcons.tsx
@@ -0,0 +1,32 @@
+function SocialIcons() {
+  return (
+    <div className="flex gap-5 justify-center">
+      <a href="https://www.facebook.com" target="_blank" rel="noopener">
+        <img
+          src="../src/assets/images/facebook.png"
+          alt="facebook logo"
+          className="w-10 h-10 bg-center bg-contain 
+            grayscale hover:grayscale-0 transition-[filter] duration-300"
+        />
+      </a>
+      <a href="https://www.telegram.org" target="_blank" rel="noopener">
+        <img
+          src="../../src/assets/images/telegram.png"
+          alt="telegram logo"
+          className="w-10 h-10 bg-center bg-contain
+            grayscale hover:grayscale-0 transition-[filter] duration-300"
+        />
+      </a>
+      <a href="https://www.twitter.com" target="_blank" rel="noopener">
+        <img
+          src="../../src/assets/images/twitter.png"
+          alt="twitter logo"
+          className="w-10 h-10 bg-center bg-contain
+            grayscale hover:grayscale-0 transition-[filter] duration-300"
+        />
+      </a>
+    </div>
+  );
+}
+
+export default SocialIcons;
